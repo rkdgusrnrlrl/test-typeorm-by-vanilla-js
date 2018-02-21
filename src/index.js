d54c925244d57ee756ded22538fccf8cd6cfc1a3
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-02-21 23:30:22 +0900

    feature:add run connect postgresql and do insert and select

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const typeorm = require("typeorm");
+const User = require("./model/User").User;
+const UserSchema = require("./entity/UserSchema");
+
+function conection() {
+
+    let UserSchema = require("./entity/UserSchema");
+    return typeorm.createConnection({
+        type : "postgres",
+        host : "localhost",
+        port : 5432,
+        username : "postgres",
+        password : "rkd123",
+        database: "test",
+        synchronize: true,
+        logging: "all",
+        entitySchemas: [
+            UserSchema
+        ]
+    });
+}
+
+async function run () {
+    try {
+        const conn = await conection();
+
+        const user = new User("rkdgusrnrlrl", "강현구", 32);
+
+
+        let userRepository = conn.getRepository(User);
+        await userRepository.save(user);
+
+        const userList = await userRepository.find();
+        console.log(userList);
+
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+run();
